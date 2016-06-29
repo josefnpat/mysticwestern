c1ec404a52619532d4c80d37bfc5f8b58a61367e
Seppi <seppi@josefnpat.com>
2016-06-29 03:31:45 -0500

    Adding basic menu w/ music

---
+++ UnityProject/Assets/Scripts/ChangeStateOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeStateOnClick : MonoBehaviour {
+
+  public string targetScene = "";
+
+  void OnMouseUp() {
+    if(targetScene == ""){
+      Debug.Log("Goodbye space cowboy.");
+      Application.Quit();
+    } else {
+      SceneManager.LoadScene(targetScene);
+    }
+  }
+
+}
