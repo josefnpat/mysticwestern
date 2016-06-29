bd98e0365d772d48711f719049e480866f1c47ab
Seppi <seppi@josefnpat.com>
2016-06-29 16:36:18 -0500

    Adding arjan's demo timer w 600 seconds

---
+++ UnityProject/Assets/Scripts/DemoTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoTimer : MonoBehaviour {
+	public float maxTimeInSeconds;
+	private float timeLeft;
+	// Use this for initialization
+	void Start () {
+		timeLeft = maxTimeInSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		if ( timeLeft < 0 )
+		{
+			 Application.Quit();
+		}
+	}
+}
