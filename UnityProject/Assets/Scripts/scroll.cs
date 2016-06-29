c5826bb7c9d45f2865357c14030ce64e0dfacba8
Seppi <seppi@josefnpat.com>
2016-06-29 17:41:49 -0500

    Adding credits crawl

---
+++ UnityProject/Assets/Scripts/scroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+	public float speed;
+	public Vector3 orientation;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(orientation * Time.deltaTime * speed);
+	}
+}
